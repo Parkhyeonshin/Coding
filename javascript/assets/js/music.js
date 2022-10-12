4f0ed0ef6a6e9ba822dcc820a73edf13559d78d2
Parkhyeonshin <sshin4882@gmail.com>
2022-10-12 18:06:37 +0900

    221012
    컴퓨터

--- javascript/assets/js/music.js
+++ javascript/assets/js/music.js
@@ -1,125 +1,226 @@
 const allMusic = [
     {
-        name : "Average - 1",
-        artist : "Patrick Patrikios",
-        img : "musicCover1",
-        audio : "musicAudio1",
+        name: "Music Track 01",
+        artist: "Various artist",
+        img: "music_view01",
+        audio: "music_audio01"
     },
-    // 01
     {
-        name : "Average - 2",
-        artist : "Patrick Patrikios",
-        img : "musicCover2",
-        audio : "musicAudio2",
+        name: "Music Track 02",
+        artist: "Various artist",
+        img: "music_view02",
+        audio: "music_audio02"
     },
-    // 02
     {
-        name : "Average - 3",
-        artist : "Patrick Patrikios",
-        img : "musicCover3",
-        audio : "musicAudio3",
+        name: "Music Track 03",
+        artist: "Various artist",
+        img: "music_view03",
+        audio: "music_audio03"
     },
-    // 03
     {
-        name : "Average - ",
-        artist : "Patrick Patrikios",
-        img : "musicCover4",
-        audio : "musicAudio4",
+        name: "Music Track 04",
+        artist: "Jeremy Black",
+        img: "music_view04",
+        audio: "music_audio04"
     },
-    // 04
     {
-        name : "Average - ",
-        artist : "Patrick Patrikios",
-        img : "musicCover5",
-        audio : "musicAudio5",
+        name: "Music Track 05",
+        artist: "Patrick Patrikios",
+        img: "music_view05",
+        audio: "music_audio05"
     },
-    // 05
     {
-        name : "Average - ",
-        artist : "Patrick Patrikios",
-        img : "musicCover6",
-        audio : "musicAudio6",
+        name: "Music Track 06",
+        artist: "DJ Freedem",
+        img: "music_view06",
+        audio: "music_audio06"
     },
-    // 06
     {
-        name : "Average - ",
-        artist : "Patrick Patrikios",
-        img : "musicCover7",
-        audio : "musicAudio7",
+        name: "Music Track 07",
+        artist: "Asher Fulero",
+        img: "music_view07",
+        audio: "music_audio07"
     },
-    // 07
     {
-        name : "Average - ",
-        artist : "Patrick Patrikios",
-        img : "musicCover8",
-        audio : "musicAudio8",
+        name: "Music Track 08",
+        artist: "Jeremy Black",
+        img: "music_view08",
+        audio: "music_audio08"
     },
-    // 08
     {
-        name : "Average - ",
-        artist : "Patrick Patrikios",
-        img : "musicCover9",
-        audio : "musicAudio9",
+        name: "Music Track 09",
+        artist: "Kwon",
+        img: "music_view09",
+        audio: "music_audio09"
     },
-    // 09
-    {
-        name : "Average - 111",
-        artist : "Patrick Patrikios",
-        img : "musicCover10",
-        audio : "musicAudio10",
-    }
-    // 10
-];
-
+]
 const musicWrap = document.querySelector(".music__wrap");
-const musicView = document.querySelector(".music__view .img img");
-const musicName  = document.querySelector(".music__view .title h3");
-const musicArtist = document.querySelector(".music__view .title p");
-const musicAudio = document.querySelector("#main-audio");
-const musicPlay = document.querySelector("#control-play");
-const musicPrevBtn = document.querySelector("#control-prev");
-const musicNextBtn = document.querySelector("#control-next");
-// const musicAudio
+const musicView = musicWrap.querySelector(".music__view .img img");
+const musicName = musicWrap.querySelector(".music__view .title h3");
+const musicArtist = musicWrap.querySelector(".music__view .title p");
+const musicAudio = musicWrap.querySelector("#main-audio");
+const musicPlay = musicWrap.querySelector("#control-play");
+const musicPrevBtn = musicWrap.querySelector("#control-prev");
+const musicNextBtn = musicWrap.querySelector("#control-next");
+const musicProgress = musicWrap.querySelector(".progress");
+const musicProgressBar = musicWrap.querySelector(".progress .bar");
+const musicProgressCurrent = musicWrap.querySelector(".progress .timer .current");
+const musicProgressDuration = musicWrap.querySelector(".progress .timer .duration");
+const musicRepeat = musicWrap.querySelector("#control-repeat");
+const musicListBtn = musicWrap.querySelector("#control-list");
+const musicList = musicWrap.querySelector(".music__list");
+const musicListUl = document.querySelector(".music__list ul");
 
-let musicIndex = 1;
 
+let musicIndex = 3;
 // 음악 재생
-function loadMusic(num){
-    musicName.innerText = allMusic[num-1].name;
-    musicArtist.innerText = allMusic[num-1].artist;
-    musicView.src = `../assets/img/${allMusic[num-1].img}.png`;
-    musicView.src = allMusic[num-1].name
-    musicAudio.src = `../assets/audio/${allMusic[num-1].audio}.mp3`;
-    // console.log(musicView.src)
+function loadMusic(num) {
+    musicName.innerText = allMusic[num - 1].name;
+    musicArtist.innerText = allMusic[num - 1].artist;
+    musicView.src = `../assets/img/${allMusic[num - 1].img}.png`;
+    musicView.alt = allMusic[num - 1].name;
+    musicAudio.src = `../assets/audio/${allMusic[num - 1].audio}.mp3`;
 }
-
-// 재생버튼
-function playMusic(){
-
+// 재생 버튼
+function playMusic() {
+    musicWrap.classList.add("paused");
+    musicPlay.setAttribute("title", "정지");
+    musicPlay.setAttribute("class", "stop");
+    musicAudio.play();
 }
-
-// 정지버튼
-function pauseMusic(){
-
+// 정지 버튼
+function pauseMusic() {
+    musicWrap.classList.remove("paused");
+    musicPlay.setAttribute("title", "재생");
+    musicPlay.setAttribute("class", "play");
+    musicAudio.pause();
 }
-
-// 이전 곡 듣기 버튼
-function prevMusic(){
-
-}
-
-// 다음 곡 듣기 버튼
-function nextMusic(){
-
-}
-
-window.addEventListener("load", () => {
+// 이전 곡 듣기 버튼(
+function prevMusic() {
+    musicIndex == 1 ? musicIndex = allMusic.length : musicIndex--;
     loadMusic(musicIndex);
+    playMusic();
+}
+// 다음 곡 듣기 버튼(
+function nextMusic() {
+    musicIndex == allMusic.length ? musicIndex = 1 : musicIndex++;
+    loadMusic(musicIndex);
+    playMusic();
+}
+// 뮤직 진행바
+musicAudio.addEventListener("timeupdate", e => {
+    // console.log(e)
+    const currentTime = e.target.currentTime;           //현재 재생되는 시간
+    const duration = e.target.duration;                 //오디오의 총 길이
+    let progressWidth = (currentTime / duration) * 100;   //전체 길에이서 현재 진행되는 시간을 백분위로 나눔
+    musicProgressBar.style.width = `${progressWidth}%`;
+    // 전체시간
+    musicAudio.addEventListener("loadeddata", () => {
+        let audioDuration = musicAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);          //전체 시간(초)을 분단위로 쪼갬
+        let totalSec = Math.floor(audioDuration % 60);          //남은 초를 저장
+        if (totalSec < 10) totalSec = `0${totalSec}`;            //초가 한 자릿수 일때 앞에 0을 붙임
+        musicProgressDuration.innerText = `${totalMin}:${totalSec}`;    //완성된 시간 문자열을 출력
+    })
+
+    // 진행시간
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec < 10) currentSec = `0${currentSec}`;        
+    musicProgressCurrent.innerText = `${currentMin}:${currentSec}`;
+    console.log(currentSec)
+
+
+    // 진행바 클릭
+    // 진행바 전체길이구한뒤 각 구간의 값(백분율형식)으로 알면 구현가능함.
+    musicProgress.addEventListener("click", (e) => {
+        let progressWidth = musicProgress.clientWidth;  // 진행바 전체 길이구하기
+        let clickedOffsetX = e.offsetX;                 // 처음 시작값(부모기준(진행바))부터 클릭한지점까지의 x값 구하기
+        let songDuration = musicAudio.duration;         // 오디오 전체 길이
+    
+        musicAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;   // 백분위로 나눈 숫자에 전체길이 곱함 이로인해 현재 재생값으로 바꿈
+    })
+});
+
+// 반복 버튼 클릭
+musicRepeat.addEventListener("click", ()=>{
+    let getAttr = musicRepeat.getAttribute("class");
+
+    switch(getAttr){
+        case "repeat" :
+            musicRepeat.setAttribute("class", "repeat_one");
+            musicRepeat.setAttribute("title", "한곡 반복");
+        break;
+        case "repeat_one" :
+            musicRepeat.setAttribute("class", "shuffle");
+            musicRepeat.setAttribute("title", "랜덤 반복");
+        break;
+        case "shuffle" :
+            musicRepeat.setAttribute("class", "repeat");
+            musicRepeat.setAttribute("title", "전체 반복");
+        break;
+    }
+})
+
+// 뮤직 리스트 버튼
+musicListBtn.addEventListener("click", () => {
+    musicList.classList.add("show");
+})
+
+
+// 뮤직 리스트 구현하기
+for(let i=0; i<allMusic.length; i++){
+    let li = `
+    <li>
+        <strong>${allMusic[i].name}</strong>
+        <em>${allMusic[i].artist}</em>
+        <span></span>
+    </li>
+    `;
+    musicListUl.innerHTML += li;
+
+}
+
+// 오디오가 끝나면
+musicAudio.addEventListener("ended", ()=> {
+    let getAttr = musicRepeat.getAttribute("class");
+
+    switch(getAttr){
+        case "repeat" : 
+            nextMusic();
+        break;
+        case "repeat_one":
+            playMusic();
+        break;
+        case "shuffle" :
+            let randomIndex = Math.floor(Math.random()*allMusic.length + 1);  //랜덤인덱스생성
+
+            do {
+                randomIndex = Math.floor(Math.random()*allMusic.length + 1);
+            } while ( musicIndex==randomIndex)
+            musicIndex = randomIndex;       // 현재 인덱스를 랜덤   인덱스 변경
+            loadMusic(musicIndex);          // 랜덤 인덱스가 반영된 현재 인덱스 값으로 음악을 다시 로드    
+            playMusic();                    // 로드한 음악을 재생
+        break;
+    }
 })
 
 
 
-// 플레이버튼
-musicPlay.addEventListener("click",()=>{
-    playMusic();
-});
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+})
+// 플레이 버튼 클릭
+musicPlay.addEventListener("click", () => {
+    const isMusicPauesd = musicWrap.classList.contains("paused");   //음악이 재생중
+    isMusicPauesd ? pauseMusic() : playMusic();
+})
+// 이전곡 버튼 클릭
+musicPrevBtn.addEventListener("click", () => {
+    prevMusic();
+})
+// 다음곡 버튼 클릭
+musicNextBtn.addEventListener("click", () => {
+    nextMusic();
+})
